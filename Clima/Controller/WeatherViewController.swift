bfa5793f9ffe7661ac875050676a05f9aa3862fc
Muhammad Shams <mr.shamssheikh@gmail.com>
2020-02-29 23:18:12 +0500

    design completed

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -16,7 +16,8 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
 
